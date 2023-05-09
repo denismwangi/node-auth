aee6fbdefddf00bb10c0b0716cd61e5b9185c3ca
denismwangi <denniske992@gmail.com>
2023-05-09 09:30:47 +0300

    setup

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const path = require('path');
+
+dotenv.config({path: './dotenv'});
+const app = express();
+const db = mysql.createConnection({
+    // host: process.env.DATABASE_HOST,
+    // user: process.env.DATABASE_USER,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: process.env.DATABASE
+
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node-auth'
+
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+app.set('view engine', 'hbs');
+
+db.connect((error) =>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("database connected...");
+    }
+})
+
+
+app.get('/', (req, res) => {
+    // res.send("home");
+    res.render("index")
+
+
+});
+
+app.listen(5000, () =>{
+    console.log("Server started on port 5000");
+});
